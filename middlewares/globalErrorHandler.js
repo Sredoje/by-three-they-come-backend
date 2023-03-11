d8c370baf8665b26d4aa8793cfa898e789195ec3
Sredoje Cutovic <sredojecutovic@gmail.com>
2023-03-11 19:30:43 +0100

    Fixing production error handling

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,4 +1,4 @@
-const AppError = require("../utils/appError");
+const AppError = require('../utils/appError');
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -20,34 +20,29 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error("ERROR 💥", err);
+    console.error('ERROR 💥', err);
 
     // 2) Send generic message
     res.status(500).json({
-      status: "error",
-      message: "Something went very wrong!",
+      status: 'error',
+      message: 'Something went very wrong!',
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
-  console.log("ERR CAUGHT IN GLOBAL MIDDLEWARE");
+  console.log('ERR CAUGHT IN GLOBAL MIDDLEWARE');
   console.log(`ERR ${err}`);
 
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+  err.status = err.status || 'error';
 
-  console.log("here");
-  if (process.env.NODE_ENV === "development") {
+  console.log('here');
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
+  } else if (process.env.NODE_ENV === 'production') {
     let error = err;
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-      error = handleValidationErrorDB(error);
-
     sendErrorProd(error, res);
   }
 };
